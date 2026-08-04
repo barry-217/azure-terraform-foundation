7d7d5ed50557adf0d9b0195476b591660d3b49cb
Barath-dev <jbarathraj7@outlook.com>
2026-08-04 14:11:47 +0530

    Add Azure infrastructure resources

---
+++ main.tf
@@ -0,0 +1,148 @@
+�r�^�f��ئ{O,y�'vî���locals {
+  resource_prefix = "${var.project_name}-${var.environment}"
+  common_tags = merge(
+    {
+      Project     = "azure-terraform-foundation"
+      Environment = var.environment
+      ManagedBy   = "Terraform"
+      Owner       = "Barath Raj J"
+    },
+    var.tags
+  )
+}
+
+resource "azurerm_resource_group" "main" {
+  name     = "rg-${local.resource_prefix}"
+  location = var.location
+  tags     = local.common_tags
+}
+
+resource "azurerm_virtual_network" "main" {
+  name                = "vnet-${local.resource_prefix}"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  address_space       = [var.vnet_address_space]
+  tags                = local.common_tags
+}
+
+resource "azurerm_subnet" "application" {
+  name                 = "snet-application"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = [var.application_subnet_prefix]
+}
+
+resource "azurerm_subnet" "management" {
+  name                 = "snet-management"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = [var.management_subnet_prefix]
+}
+
+resource "azurerm_network_security_group" "application" {
+  name                = "nsg-application-${local.resource_prefix}"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  tags                = local.common_tags
+}
+
+resource "azurerm_subnet_network_security_group_association" "application" {
+  subnet_id                 = azurerm_subnet.application.id
+  network_security_group_id = azurerm_network_security_group.application.id
+}
+
+resource "azurerm_log_analytics_workspace" "main" {
+  name                = "log-${local.resource_prefix}"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+  tags                = local.common_tags
+}
+
+resource "azurerm_storage_account" "main" {
+  name                            = "st${replace(local.resource_prefix, "-", "")}"
+  resource_group_name             = azurerm_resource_group.main.name
+  location                        = azurerm_resource_group.main.location
+  account_tier                    = "Standard"
+  account_replication_type        = "LRS"
+  min_tls_version                 = "TLS1_2"
+  https_traffic_only_enabled      = true
+  allow_nested_items_to_be_public = false
+  public_network_access_enabled   = false
+  tags                            = local.common_tags
+}
+
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_key_vault" "main" {
+  name                          = "kv-${local.resource_prefix}"
+  location                      = azurerm_resource_group.main.location
+  resource_group_name           = azurerm_resource_group.main.name
+  tenant_id                     = data.azurerm_client_config.current.tenant_id
+  sku_name                      = "standard"
+  rbac_authorization_enabled   = true
+  public_network_access_enabled = false
+  soft_delete_retention_days    = 7
+  purge_protection_enabled      = false
+  tags                          = local.common_tags
+}
+
+resource "azurerm_network_interface" "vm" {
+  count               = var.deploy_vm ? 1 : 0
+  name                = "nic-admin-${local.resource_prefix}"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  tags                = local.common_tags
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.application.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "admin" {
+  count               = var.deploy_vm ? 1 : 0
+  name                = "vm-admin-${local.resource_prefix}"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  size                = "Standard_B1s"
+  admin_username      = var.admin_username
+  network_interface_ids = [
+    azurerm_network_interface.vm[0].id
+  ]
+  tags = local.common_tags
+
+  admin_ssh_key {
+    username   = var.admin_username
+    public_key = var.admin_ssh_public_key
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "ubuntu-24_04-lts"
+    sku       = "server"
+    version   = "latest"
+  }
+}
+
+resource "azurerm_monitor_diagnostic_setting" "vm" {
+  count                      = var.deploy_vm ? 1 : 0
+  name                       = "diag-vm-admin"
+  target_resource_id         = azurerm_linux_virtual_machine.admin[0].id
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.main.id
+
+  enabled_log {
+    category_group = "allLogs"
+  }
+
+  enabled_metric {
+    category = "AllMetrics"
+  }
+}
